aa3997a2bfe3295d7df82d5e3e4422d188b1a194
ophaj <ajayi.philip@outlook.com>
2024-06-15 05:42:50 +0000

    magic file trials

---
+++ 0x00-shell_basics/detect_school_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MAGIC_FILE="alx-system_engineering-devops/0x00-shell_basics/school.mgc"
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <file>"
+	exit 1
+fi
+
+FILE=$1
+
+if [ ! -f "$FILE" ]; then
+	echo "File not found!"
+	exit 1
+fi
+
+file --magic-file "$MAGIC_FILE" "$FILE"
